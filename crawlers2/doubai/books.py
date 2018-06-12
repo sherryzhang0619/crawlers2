e0cea30c70cb1380ace0ccb507e9df3d8d8dec87
sherryzhang0619 <57205694@qq.com>
2018-06-12 16:54:52 +0800

    add feature: search books and rate by page

--- crawlers2/doubai/books.py
+++ crawlers2/doubai/books.py
@@ -22,40 +22,37 @@ for i in tag: #遍历所有满足tag的xpath的路径, i也是xpath
 
     subcategory = i.xpath('table[@class="tagCol"]/tbody/tr/td/a/text()')
     for n in subcategory: #因为每个子标签是在一个table里，同一个xpath下有多个td，所以要再次遍历查找
-        print "subcategory:" + n
+        print "subcategory: %s" %n
 
         url_books = "https://book.douban.com/tag/" + "%s" %n # 用n代入到url里打开新的列表
         r = requests.get(url_books).content
         sel = html.fromstring(r)
 
-        subtitle = sel.xpath('//div[@id="content"]/h1/text()')
-        print "subtitle:" + subtitle[0] #每个子分类的标题
+        subtitle = sel.xpath('//div[@id="content"]/h1/text()')[0]
+        print "subtitle:" + subtitle #每个子分类的标题
 
+        #分页查找
+        for i in range(100):
+            num = i*20
+            page_url = "https://book.douban.com/tag/%s?start=%d&type=T" % (n, num)
+            print page_url
 
-        sublist = sel.xpath('//ul[@class="subject-list"]/li[@class="subject-item"]')
-        global list
-        list = []
-        for a in sublist:
-            books = a.xpath('div[@class="info"]/h2[@class=""]/a/text()')
-            books_name = books[0].replace(" ", "").replace("\n", "")
-            score_path = a.xpath('div[@class="info"]/div[@class="star clearfix"]/span[@class="rating_nums"]/text()')
-            # score = float(score_path[0])
-            list2 = [books_name, score_path[0]]
-            list.append(list2)
-            # print str(list2).decode('unicode-escape').replace('u','')
-        print str(list).decode('unicode-escape').replace('u','')
+            #每页上查找图书及对应的评分
 
-            # print books_name, score
-        #     list = list.append(list2)
-        # print str(list).decode('string_escape')  #用str(list_name).decode('string_escape')来解决list中文不显示的问题
+            r = requests.get(page_url).content
+            sel = html.fromstring(r)
 
-
-
-
-
-
-
- # str(update_list).decode('string_escape')
+            #将子分类中的图书和评分存到列表list中
+            sublist = sel.xpath('//ul[@class="subject-list"]/li[@class="subject-item"]')
+            list = []
+            for a in sublist:
+                books = a.xpath('div[@class="info"]/h2[@class=""]/a/text()')
+                books_name = books[0].replace(" ", "").replace("\n", "")
+                score_path = a.xpath('div[@class="info"]/div[@class="star clearfix"]/span[@class="rating_nums"]/text()')
+                # score = float(score_path[0])
+                list2 = [books_name, score_path[0]]
+                list.append(list2) #不能写成list = list.append(list2)
+            print str(list).decode('unicode-escape').replace('u','')
 
 
 
